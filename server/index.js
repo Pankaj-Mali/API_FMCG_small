4f88fc28d3d30d19326d2012f08feb19b8532134
Pankaj-Mali <malipankaj660@gmail.com>
2022-11-18 20:56:00 +0530

    Route added to handle operations by employee

--- server/index.js
+++ server/index.js
@@ -7,6 +7,8 @@ const cors = require("cors");
 const google = require("./Routes/userGoogleLogin")
 const userLoginResister= require("./Routes/userEntry")
 const employeeLogin = require("./Routes/employeeLogin")
+const employeeOperations = require("./Routes/employeeOperations")
+const adminOperations = require("./Routes/admin")
 
 const secrete = "pankaj@98+27*3"
 
@@ -27,6 +29,13 @@ app.use("/api/landingPage/user" , userLoginResister);
 
 // for employee login
 app.use("/api/landingPage/employee/" , employeeLogin);
+
+// operations done by employee
+app.use("/api/employee/" , employeeOperations);
+
+//for the operations done by admin only
+app.use("app/employee/admin" , adminOperations);
+
 
 
 app.listen(8080, (e) => {
